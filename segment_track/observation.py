ee93feb3f5856b68c09079f36f1c22b34e490666
Mason Peterson <mbpeterson70@gmail.com>
2024-03-01 17:33:12 -0500

    initial segment tracking working
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

---
+++ segment_track/observation.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class Observation():
+    """
+    Segment observation data class
+    """
+
+    time: float
+    pose: np.ndarray
+    pixel: np.ndarray
+    width: int
+    height: int
+    mask: np.ndarray = None
+
+    def copy(self, include_mask: bool = True):
+        if include_mask:
+            return Observation(self.time, self.pose.copy(), self.pixel.copy(), 
+                               self.width, self.height, self.mask.copy())
+        else:
+            return Observation(self.time, self.pose.copy(), self.pixel.copy(), 
+                           self.width, self.height, None)
