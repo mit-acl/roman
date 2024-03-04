6ca2aea742bfcb92c4ec1595d79141c2e3425005
Mason Peterson <mbpeterson70@gmail.com>
2024-03-04 10:04:06 -0500

    add setup.py and ability to compute objects width/height
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- segment_track/segment.py
+++ segment_track/segment.py
@@ -2,6 +2,7 @@ import numpy as np
 import gtsam
 
 from robot_utils.robot_data.img_data import CameraParams
+from robot_utils.transform import transform
 
 from segment_track.observation import Observation
 
@@ -16,6 +17,7 @@ class Segment():
             self.last_mask = None
         self.observations = [observation.copy(include_mask=False)]
         self.last_seen = observation.time
+        self.camera_params = camera_params
         self.cal3ds2 = gtsam.Cal3DS2(camera_params.K[0,0], camera_params.K[1,1], 
             camera_params.K[0,1], camera_params.K[0,2], camera_params.K[1,2], 
             camera_params.D[0], camera_params.D[1], camera_params.D[2], camera_params.D[3])
@@ -29,7 +31,7 @@ class Segment():
         self.last_seen = observation.time
         self.num_sightings += 1
 
-    def reconstruction3D(self):
+    def reconstruction3D(self, width_height=False):
         camera_poses = []
         for obs in self.observations:
             camera_poses.append(gtsam.PinholeCameraCal3DS2(gtsam.Pose3(obs.pose), self.cal3ds2))
@@ -37,4 +39,13 @@ class Segment():
         pixels_point_vec = gtsam.Point2Vector([gtsam.Point2(obs.pixel) for obs in self.observations])
         measurement_noise = gtsam.noiseModel.Isotropic.Sigma(2, self.pixel_std_dev)
         reconstruction = gtsam.triangulatePoint3(camera_set, pixels_point_vec, rank_tol=1e-9, optimize=True, model=measurement_noise)
+
+        if width_height:
+            ws, hs = [], []
+            for obs in self.observations:
+                p_c = transform(np.linalg.inv(obs.pose), reconstruction)
+                ws.append(obs.width * np.abs(p_c[0]) / np.abs(obs.pixel[0] - self.camera_params.K[0,2]))
+                hs.append(obs.height * np.abs(p_c[1]) / np.abs(obs.pixel[1] - self.camera_params.K[1,2]))
+            return np.concatenate((reconstruction, [np.mean(ws)], [np.mean(hs)]))
+                
         return reconstruction
