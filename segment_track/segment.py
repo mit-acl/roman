f22c0131db54b0f299e51572d4f5bcdb982848f8
Mason Peterson <mbpeterson70@gmail.com>
2024-03-19 16:08:38 -0400

    add methods for reconstructing mask and covariance of segment
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- segment_track/segment.py
+++ segment_track/segment.py
@@ -3,6 +3,7 @@ import gtsam
 
 from robot_utils.robot_data.img_data import CameraParams
 from robot_utils.transform import transform
+from robot_utils.camera import xyz_2_pixel
 
 from segment_track.observation import Observation
 
@@ -35,6 +36,8 @@ class Segment():
     def reconstruction3D(self, width_height=False):
 
         if not self.edited and (not width_height or len(self.reconstruction) > 3):
+            if not width_height:
+                return self.reconstruction[:3]
             return self.reconstruction
         
         camera_poses = []
@@ -55,3 +58,51 @@ class Segment():
                 
         self.reconstruction = reconstruction
         return reconstruction
+    
+    def reconstruct_mask(self, pose):
+        mask = np.zeros((self.camera_params.height, self.camera_params.width), dtype=np.uint8)
+        reconstruction = self.reconstruction3D(width_height=True)
+        p_c = transform(np.linalg.inv(pose), reconstruction[:3])
+        if p_c[2] < 0:
+            return mask
+        
+        w, h = reconstruction[3], reconstruction[4]
+        lower_left_c = p_c - np.array([w/2, h/2, 0])
+        upper_right_c = p_c + np.array([w/2, h/2, 0])
+        lower_left_uv = xyz_2_pixel(lower_left_c, self.camera_params.K)
+        upper_right_uv = xyz_2_pixel(upper_right_c, self.camera_params.K)
+
+        lower_left_uv = np.round(lower_left_uv).astype(int).reshape(-1)
+        upper_right_uv = np.round(upper_right_uv).astype(int).reshape(-1)
+
+        lower_left_uv = np.maximum(lower_left_uv, 0)
+        upper_right_uv = np.minimum(upper_right_uv, [self.camera_params.width, self.camera_params.height])
+        mask[lower_left_uv[1]:upper_right_uv[1], lower_left_uv[0]:upper_right_uv[0]] = 1
+
+        return mask
+    
+    @property
+    def covariance(self):
+        graph = gtsam.NonlinearFactorGraph()
+        initial_guess = gtsam.Values()
+        segment = gtsam.symbol_shorthand.S(0)
+        x = gtsam.symbol_shorthand.X
+
+        for i, obs in enumerate(self.observations):
+
+            pose_factor = gtsam.NonlinearEqualityPose3(x(i), gtsam.Pose3(obs.pose))
+            graph.push_back(pose_factor)
+            initial_guess.insert(x(i), gtsam.Pose3(obs.pose))
+
+            measurement_noise = gtsam.noiseModel.Isotropic.Sigma(2, self.pixel_std_dev)
+            projection_factor = gtsam.GenericProjectionFactorCal3DS2(
+                gtsam.Point2(obs.pixel), measurement_noise, x(i), segment, self.cal3ds2)
+            graph.push_back(projection_factor)
+        initial_guess.insert(segment, gtsam.Point3(self.reconstruction3D(width_height=False)))
+
+        optimizer = gtsam.GaussNewtonOptimizer(graph, initial_guess)
+
+        result = optimizer.optimize()
+        marginal_covariance = gtsam.Marginals(graph, result)
+
+        return marginal_covariance.marginalCovariance(segment)
