Fix segment world width/height computation
Previously, this was computed using the ratio between the (pixel
center and the camera center distance) and the (world point value
and origin distance) - the problem here is that if the object was
near the origin along either x or y axis, the ratio became ill-
conditioned.

Now, this is computed using the ratio between focal distance and
object depth.

Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

segment_track/segment.py
@@ -72,8 +72,8 @@ class Segment():
             ws, hs = [], []
             for obs in observations:
                 p_c = transform(np.linalg.inv(obs.pose), reconstruction)
-                ws.append(obs.width * np.abs(p_c[0]) / np.abs(obs.pixel[0] - self.camera_params.K[0,2]))
-                hs.append(obs.height * np.abs(p_c[1]) / np.abs(obs.pixel[1] - self.camera_params.K[1,2]))
+                ws.append(obs.width * np.abs(p_c[2]) / self.camera_params.K[0,0])
+                hs.append(obs.height * np.abs(p_c[2]) / self.camera_params.K[1,1])
             reconstruction = np.concatenate((reconstruction, [np.mean(ws)], [np.mean(hs)]))
                 
         self.reconstruction = reconstruction