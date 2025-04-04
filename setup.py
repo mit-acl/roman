8d5f9fb91343567c91174215a58f658636e8ba40
Mason Peterson <mbpeterson70@gmail.com>
2025-04-04 11:01:32 -0400

    update package requirements

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'matplotlib', 
         'gtsam',
         'scikit-image',
-        'open3d==0.18.0',
+        'open3d>=0.18.0',
         'yolov7-package',
         'shapely',
         'opencv-python>=4.6.0',
