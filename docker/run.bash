1f53d7bbc564f9566df0a2ca1493a3d763e693a1
Mason Peterson <mbpeterson70@gmail.com>
2026-01-19 11:01:53 -0500

    update docker to build Kimera-RPGO

--- docker/run.bash
+++ docker/run.bash
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
-# ROS_VARS="-e ROS_MASTER_URI=$ROS_MASTER_URI -e ROS_IP=$ROS_IP -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
-# KIMERA_VARS="-e CAMERA=$KIMERA_VIO_CAMERA -e ROBOT_NAME=$ROBOT_NAME"
+# Usage: ./run.bash [data_dir] [results_dir] [params_dir] [gpu_arg]
+# Mounts data_dir to /root/data
+# Mounts results_dir to /root/results
+# Mounts params_dir to /root/params
+# gpu_arg examples: " " (no gpu), "--gpus all"
 
-VOLUMES="-v $ROMAN_DEMO_DATA:/roman_demo_data"
-VARS="-e ROMAN_DEMO_DATA=/roman_demo_data -e ROMAN_WEIGHTS=/roman/weights"
+export DATA_DIR=${1:-$ROMAN_DEMO_DATA}
+export RESULTS_DIR=${2:-"./"}
+export PARAMS_DIR=${3:-"./params"}
+export GPU_ARG=${4:-"--gpus all"}
+
+VOLUMES="-v $DATA_DIR:/root/data -v $RESULTS_DIR:/root/results -v $PARAMS_DIR:/root/params"
+VARS="-e ROMAN_DEMO_DATA=/root/data -e ROMAN_WEIGHTS=/root/roman/weights"
 
 docker run -it --rm --net=host \
-    $VOLUMES $VARS --gpus all roman
+    $VOLUMES $VARS $GPU_ARG roman
